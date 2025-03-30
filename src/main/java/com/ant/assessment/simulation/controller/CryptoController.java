cf87cb75b839df9f32de726775fd833014dcc719
khoi.nguyen1 <khoi.nguyen1@mservice.com.vn>
2025-03-30 08:09:41 +0700

    deploy

--- src/main/java/com/ant/assessment/simulation/controller/CryptoController.java
+++ src/main/java/com/ant/assessment/simulation/controller/CryptoController.java
@@ -3,10 +3,12 @@ package com.ant.assessment.simulation.controller;
 import com.ant.assessment.simulation.component.RSAUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CryptoController {
 
     @Autowired
